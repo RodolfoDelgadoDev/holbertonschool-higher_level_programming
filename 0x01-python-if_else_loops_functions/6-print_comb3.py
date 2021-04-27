ba0f30813dc34385775d1c28ab25fceeabff4941
Ro8s <rodrigo.delgado3132@gmail.com>
2021-04-27 20:56:12 +0000

    fix 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(9):
-    for j in range (10):
+    for j in range(10):
         if i < 1 + j and i != j and (j + i != 17):
             print("{}{}, ".format(i, j), end="")
 print("89")
