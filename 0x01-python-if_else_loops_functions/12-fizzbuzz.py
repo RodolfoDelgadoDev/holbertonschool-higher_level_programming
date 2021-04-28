b158123e79729887d8d22ab54fa1f0b536bb2b30
Ro8s <rodrigo.delgado3132@gmail.com>
2021-04-28 01:25:52 +0000

    addded ex 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1,101):
+        if (i % 3) == 0 and (i % 5) == 0:
+            print("FizzBuzz ".format(), end="")
+            continue
+        elif (i % 3) == 0:
+            print("Fizz ".format(), end="")
+        elif (i % 5) == 0:
+            print("Buzz ".format(), end="")
+            continue
+        else:
+            print("{} ".format(i), end="")
