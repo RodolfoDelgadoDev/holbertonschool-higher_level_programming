f8db82d147b924db54a10b726969809ab06182e1
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-05 01:36:53 +0000

    add ex 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) > 0:
+        a = my_list[0]
+        if len(my_list) == 1:
+            return a
+        for i in range(1, len(my_list)):
+            if my_list[i] > a:
+                a = my_list[i]
+    return a
