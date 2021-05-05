b146fadefb900ca0f9fc4955bc997e1e0014e098
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-05 01:12:30 +0000

    added 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return(0, None)
+    length = len(sentence)
+    first = sentence[0]
+    return (length, first)
