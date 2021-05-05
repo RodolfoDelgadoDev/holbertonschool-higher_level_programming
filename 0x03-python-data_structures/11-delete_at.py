9a3940aaf7fbd8d22a94f3108bf0956716793ed9
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-05 02:09:17 +0000

    added ex 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list) - 1:
+        return my_list
+    dele = []
+    for i in range(len(my_list)):
+        if i != idx:
+            dele.append(my_list[i])
+    return dele
