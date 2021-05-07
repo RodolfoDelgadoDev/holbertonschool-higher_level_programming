876e322790064afe48e1dff8a9fd8fbdc868cf62
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-07 02:25:04 +0000

    pichula confusa

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,20 +2,23 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
-    ro= {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    ro = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     con = 0
     rev = False
     lon = len(roman_string)
     for n in range(lon):
-        if rev == True:
+        if rev is True:
             rev = False
-            if n < lon: 
+            if n < lon:
                 continue
             else:
                 break
-        con =  con + ro.get(roman_string[n])
-        if n + 1 < lon:
-            if  ro.get(roman_string[n+1]) > ro.get(roman_string[n]):
+        if n < lon - 1:
+            if ro.get(roman_string[n+1]) > ro.get(roman_string[n]):
                 con = con + ro.get(roman_string[n + 1]) - 1
                 rev = True
+            else:
+                con = con + ro.get(roman_string[n])
+        else:
+            con = con + ro.get(roman_string[n])
     return con
