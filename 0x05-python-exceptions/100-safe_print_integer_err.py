3c65b7a3bc1f5e1b2de106fbe6a1789b8d298c2f
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-17 23:58:24 +0000

    added ex 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as err:
+        print("Exception:", err, file=sys.stderr)
+        return False
+    return True
