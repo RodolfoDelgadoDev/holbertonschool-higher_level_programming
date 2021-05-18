db20adf5ded9deb93d51dbf2c18e9277686798f7
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-18 20:45:08 +0000

    fix pep8

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square():
     '''size validation'''
     def __init__(self, size=0):
         check = isinstance(size, (int))
-        if check == False:
+        if check is False:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
