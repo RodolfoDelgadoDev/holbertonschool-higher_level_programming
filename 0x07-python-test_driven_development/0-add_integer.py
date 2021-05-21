1c2de85383c1ff753d51f33f189644ba9fb9ea82
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-21 05:44:09 +0000

    added ex 0 and README

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Function'''
+
+
+def add_integer(a, b=98):
+    '''Def addition between to int'''
+    if type(a) not in [int, float]: 
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
