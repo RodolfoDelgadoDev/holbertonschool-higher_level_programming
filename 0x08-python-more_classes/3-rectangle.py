58736dd795b180dceffb0d0fee33d585e9f5ea06
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-24 22:53:45 +0000

    add ex 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+''' Functions '''
+
+
+class Rectangle():
+    '''class Rectangle'''
+
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    ''' def retrive width '''
+
+    @property
+    def width(self):
+        return self.__width
+
+    ''' def setter width '''
+    
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    ''' def retrive height '''
+
+    @property
+    def height(self):
+        return self.__height
+
+    '''def setter width'''
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    ''' area function '''
+
+    def area(self):
+        ar = self.__height * self.__width
+        return ar
+
+    ''' perimeter function '''
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        perim = 2 * (self.__height + self.__width)
+        return perim
+
+    ''' function that prints '''
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        are = self.__height * self.__width
+        cad = "#" * self.__width
+        for i in range(1, are - self.__width):
+            cad = cad + '\n'
+            cad = cad + ("#" * self.__width)
+        return cad
+
