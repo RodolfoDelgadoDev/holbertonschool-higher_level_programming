90ea072bd3fe5f026a6ea3611c4f8feb9e050c33
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-24 22:45:32 +0000

    fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle():
     '''class Rectangle'''
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     ''' def retrive width '''
 
