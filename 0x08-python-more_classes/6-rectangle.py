8071dc1bd11b04b726abe3bdea7c0a9753d7e901
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-25 00:08:52 +0000

    algo xd

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,14 +3,16 @@
 
 
 class Rectangle():
-    number_of_instances = 0
     '''class Rectangle'''
+
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
 
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+
     ''' def retrive width '''
 
     @property
