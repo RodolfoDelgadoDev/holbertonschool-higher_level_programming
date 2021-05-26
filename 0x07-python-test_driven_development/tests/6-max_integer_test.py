114da7878b8b2ca07a703f571851b12f3150365a
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-26 13:16:36 +0000

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,14 +7,14 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     ''' Test positives '''
-    def positives(self):
+    def test_positives(self):
         self.assertEqual([100, 101], 101)
         self.assertEqual([1], 1)
         self.assertEqual([0], 0)
 
     ''' Test negatives '''
 
-    def negatives(self):
+    def test_negatives(self):
         self.assertEqual([-1, -2, -3], -1)
         self.assertEqual([1, 2, -1, 3], 3)
         self.assertEqual([-1, 0, -3, 5], 5)
