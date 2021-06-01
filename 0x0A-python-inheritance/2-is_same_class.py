c1315c1acc6bc742b08ced23d19fa3ed0c981b8b
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-01 23:12:49 +0000

    add ex 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Function'''
+
+
+def is_same_class(obj, a_class):
+    ''' is same class '''
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
