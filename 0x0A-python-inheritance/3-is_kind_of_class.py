26bd5abe529896eaa78f833cc5e304362012676e
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-01 23:44:55 +0000

    fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     ''' is kind of class '''
-    if type(obj) is a_class or issubclass(a_class, type(obj)):
+    if type(obj) is a_class or issubclass(type(obj), a_class):
         return True
     else:
         return False
