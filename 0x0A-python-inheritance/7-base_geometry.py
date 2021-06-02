1e8cf3f75544615029e8c8c122f4568bdd16a835
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-02 00:42:13 +0000

    add ex 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Classes '''
+
+
+class BaseGeometry:
+    ''' Base geometry '''
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    ''' integer validator '''
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
