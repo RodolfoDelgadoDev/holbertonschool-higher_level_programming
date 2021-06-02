d08c363c4c55229d067d4cacfac22e5cb4fb1e72
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-02 20:42:28 +0000

    no tiene sentido

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+''' Module '''
+
+
 def write_file(filename="", text=""):
+    ''' write file '''
     with open(filename, mode="w+", encoding="utf-8") as File:
         c = 0
         for i in text:
