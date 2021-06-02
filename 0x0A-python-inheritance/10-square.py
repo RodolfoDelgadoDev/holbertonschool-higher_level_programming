6a58fb0bf7aa56e8d63c6ca2b6cc319d8b29922f
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-02 02:14:11 +0000

    add ex 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Square'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Square'''
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    ''' area '''
+    def area(self):
+        return self.__size ** 2
