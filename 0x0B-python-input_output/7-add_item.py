da4635e6bfd6da9d046291825bdbc2f3828f9942
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-02 22:16:12 +0000

    ulima de todas

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-''' File add item '''
+""" class Student """
 
 
 import json
-import sys
 import os.path
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-''' start '''
-f = 'add_item.json'
-if (os.path.isfile(f)):
-    obj = load_from_json_file(f)
+myfile = "add_item.json"
+if (os.path.isfile(myfile)):
+    my_list = load_from_json_file(myfile)
 else:
-    obj = []
+    my_list = []
+
 for i in range(1, len(sys.argv)):
-    obj = obj.append(sys.argv[i])
-save_to_json_file(obj, f)
+    my_list.append(sys.argv[i])
+
+save_to_json_file(my_list, myfile)
+
