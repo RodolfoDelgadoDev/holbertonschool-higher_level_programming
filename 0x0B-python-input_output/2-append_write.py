72b779092cd15b17b76de65b150d34598dfadc07
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-02 20:43:59 +0000

    no tiene sentido

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+''' Module '''
+
+
 def append_write(filename="", text=""):
+    ''' add to the end '''
     with open(filename, mode="a", encoding="utf-8") as File:
         File.write(text)
         c = 0
