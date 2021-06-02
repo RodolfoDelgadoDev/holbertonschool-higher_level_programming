b902d66126d96b5dc51079531ce49f4d6d03aa6d
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-02 22:04:16 +0000

    a

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,11 +9,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 ''' start '''
-File = 'add_item.json'
-if (os.path.isfile(File)):
-    obj = load_from_json_file(File)
+f = 'add_item.json'
+if (os.path.isfile(f)):
+    obj = load_from_json_file(f)
 else:
     obj = []
 for i in range(1, len(sys.argv)):
     obj = obj.append(sys.argv[i])
-save_to_json_file(obj, File)
+save_to_json_file(obj, f)
