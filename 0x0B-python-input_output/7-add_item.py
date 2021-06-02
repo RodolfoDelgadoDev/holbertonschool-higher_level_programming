546a2a2bf8ba3e2fd0e305521e53c6ef4d6d26f2
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-02 21:55:32 +0000

    fiks

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,8 @@
 import json
 import sys
 import os
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 File = 'add_item.json'
 if os.path.exists(File):
