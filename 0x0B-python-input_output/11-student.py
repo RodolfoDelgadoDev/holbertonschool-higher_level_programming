70379520c81ebfa23d48a12c7eca1ca2f0d55759
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-03 00:20:11 +0000

    ex 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' Module '''
+
+
+class Student:
+    '''Student init'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    ''' to json '''
+    def to_json(self, attrs=None):
+        dic = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if type(i) is not str:
+                    return (self.__dict__)
+            for j in attrs:
+                if j in self.__dict__:
+                    dic[j] = self.__dict__[j]
+            return dic
+        return (self.__dict__)
+    ''' reload from json '''
+    def reload_from_json(self, json):
+        for i in json:
+            if i in self.__dict__:
+                self.__dict__.update({i: json[i]})
