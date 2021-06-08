ca6b42d6362cc6d5475fbba7413473a9d36b23f4
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-08 23:21:48 +0000

    a

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,4 +21,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         ''' to json string '''
+        if list_dictionaries is None:
+            return "[]"
         return json.dumps(list_dictionaries)
