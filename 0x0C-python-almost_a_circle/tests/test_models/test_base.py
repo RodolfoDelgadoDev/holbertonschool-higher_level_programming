108e8af1ccfbec8ee5b54071664ead9d4710b05a
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-08 19:22:34 +0000

    xD

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unittest for base
+"""
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    ''' Test Task 1 '''
+    def test_BaseT0(self):
+        b = Base(3)
+        self.assertEqual(b.id, 3)
+        b = Base(100)
+        self.assertEqual(b.id, 100)
+        b = Base(None)
+        self.assertEqual(b.id, 1)
+        b = Base(None)
+        self.assertEqual(b.id, 2)
+        b = Base()
+        self.assertEqual(b.id, 3)
+        b = Base(-3)
+        self.assertEqual(b.id, -3)
+        b = Base()
+        self.assertEqual(b.id, 4)
+
+if __name__ == '__main__':
+    unittest.main()
