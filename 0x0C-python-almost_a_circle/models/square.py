4b07e46fd52f721d803d6773ad54a97af9b1dbc2
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-08 20:55:06 +0000

    add ex 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,12 +7,13 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     ''' Square '''
+
     def __init__(self, size, x=0, y=0, id=None):
         ''' init '''
         super().__init__(size, size, x, y, id)
-        self.size = size
 
     ''' __str__ '''
+
     def __str__(self):
         ''' str '''
         a = self.id
@@ -20,3 +21,18 @@ class Square(Rectangle):
         c = self.y
         d = self.size
         return "[Square] ({}) {}/{} - {}".format(a, b, c, d)
+
+    ''' property setter '''
+
+    @property
+    def size(self):
+        ''' size '''
+        return self.width
+
+    ''' setter propery '''
+
+    @size.setter
+    def size(self, value):
+        ''' size '''
+        self.width = value
+        self.height = value
