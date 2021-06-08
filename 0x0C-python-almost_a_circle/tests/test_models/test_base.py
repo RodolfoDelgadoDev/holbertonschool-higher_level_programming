54d4fc333e12e7d5ed58ab0a88d3f90e028965ed
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-08 19:30:19 +0000

    esto

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,22 +7,25 @@ from models.base import Base
 
 class TestBase(unittest.TestCase):
     ''' Test Task 1 '''
+
     def test_BaseT0(self):
         ''' test '''
         b = Base(3)
         self.assertEqual(b.id, 3)
-        b = Base(100)
-        self.assertEqual(b.id, 100)
+
+    ''' Test Task 1 '''
+
+    def test_BaseNone(self):
+        ''' test '''
         b = Base(None)
         self.assertEqual(b.id, 1)
-        b = Base(None)
-        self.assertEqual(b.id, 2)
-        b = Base()
-        self.assertEqual(b.id, 3)
+
+    ''' Test Task 1 '''
+
+    def test_BaseNegative(self):
+        ''' test '''
         b = Base(-3)
         self.assertEqual(b.id, -3)
-        b = Base()
-        self.assertEqual(b.id, 4)
 
 if __name__ == '__main__':
     unittest.main()
