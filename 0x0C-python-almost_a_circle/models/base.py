768dc5dae1db59704b615c1962f20f6968b495bc
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-08 23:31:10 +0000

    caso borde

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         ''' to json string '''
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
