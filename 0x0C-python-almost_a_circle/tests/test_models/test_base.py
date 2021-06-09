658cef12fbc0a4bd894c67eb0e573714b025e509
Ro8s <rodrigo.delgado3132@gmail.com>
2021-06-09 02:24:22 +0000

    niidea

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Unittest for base
 """
+
+
 import unittest
 from models.base import Base
 
@@ -8,16 +10,31 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     ''' Test Task 1 '''
 
-    def test_BaseT1(self):
+    def test_BaseT0(self):
         ''' test '''
         b = Base(3)
         self.assertEqual(b.id, 3)
+
+    ''' Test Task 1 '''
+
+    def test_BaseNone(self):
+        ''' test '''
         b = Base(None)
-        self.assertEqual(b.id, 1)
+        self.assertEqual(b.id, 2)
+
+    ''' Test Task 1 '''
+
+    def test_BaseNegative(self):
+        ''' test '''
         b = Base(-3)
         self.assertEqual(b.id, -3)
+
+    ''' Test Task 1 '''
+
+    def test_BaseEmpty(self):
+        ''' test '''
         b = Base()
-        self.assertEqual(b.id, 2)
+        self.assertEqual(b.id, 1)
 
 if __name__ == '__main__':
     unittest.main()
