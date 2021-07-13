0c8310c45c8b1cb85ac06091cc1e519c50c04f7b
Ro8s <rodrigo.delgado3132@gmail.com>
2021-07-13 23:57:03 +0000

    added ex 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- script that lists all cities contained in the database hbtn_0d_usa
+-- script
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+INNER JOIN states 
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
