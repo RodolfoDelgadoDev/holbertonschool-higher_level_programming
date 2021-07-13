aec46ee986a380caae9f617aa6e94a8aa9690878
Ro8s <rodrigo.delgado3132@gmail.com>
2021-07-13 16:06:29 +0000

    fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,5 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 SHOW GRANTS FOR user_0d_1@localhost;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 SHOW GRANTS FOR user_0d_2@localhost;
