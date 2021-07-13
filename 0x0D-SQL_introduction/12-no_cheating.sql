1d524e429b4aeec782301fdd12012eac1ab4299f
Ro8s <rodrigo.delgado3132@gmail.com>
2021-07-13 00:41:28 +0000

    fix commits

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
--- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- script that updates the score of Bob to 10 in the table second_table.
 UPDATE second_table SET score = 10 WHERE name = 'Bob';
