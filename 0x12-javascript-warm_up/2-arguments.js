574df0f0ada118837efd3fa9e9275e729f0d17ee
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-02 16:18:24 +0000

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 if (process.argv[2]) {
-  console.log('Argument found');
+  if (process.argv[3]) {
+    console.log('Arguments found');
+  } else {
+    console.log('Argument found');
+  }
 } else {
   console.log('No argument');
 }
