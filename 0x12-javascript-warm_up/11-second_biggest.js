2c398f30c37444dd231618d4801f6b423eda6597
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-02 22:30:41 +0000

    added ex 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv;
+if (isNaN(args[3])) {
+  console.log(0);
+} else {
+  let pichu = 3;
+  let max = args[3];
+  let secmax = args[4];
+  while (!isNaN(args[pichu])) {
+    if (max < args[pichu]) {
+      secmax = max;
+      max = args[pichu];
+    }
+    pichu++;
+  }
+  console.log(secmax);
+}
