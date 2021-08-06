6f4ffaa6273bb6d5aa68d4d35dd5959ad945141d
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-06 02:56:22 +0000

    added ex 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Script that lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+import sys
+
+'''Open database conection'''
+
+database = MySQLdb.connect("localhost", sys.argv[1],
+                           sys.argv[2], sys.argv[3], port=3306)
+
+'''Cursor object with cursor() method'''
+cursor = database.cursor()
+
+'''Take the table and execute commands with execute() method'''
+constable = """SELECT *
+        FROM states
+        ORDER BY id ASC"""
+
+cursor.execute(constable)
+
+'''Get the results with method fetchall()'''
+
+results = cursor.fetchall()
+
+'''Print the results'''
+
+for raw in results:
+    print(raw)
+
+'''Disconect'''
+database.close()
