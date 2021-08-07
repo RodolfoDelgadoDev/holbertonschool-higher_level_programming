815d830e5bc7f8529e3250dbdb47fc64932c2066
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-07 20:26:24 +0000

    added ex 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''Script that lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+import sys
+
+'''Open database conection'''
+
+database = MySQLdb.connect("localhost", sys.argv[1],
+                           sys.argv[2], sys.argv[3], port=3306)
+
+cuar = sys.argv[4]
+'''Cursor object with cursor() method'''
+cursor = database.cursor()
+
+'''Consulte the table and execute commands with execute() method'''
+cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+               {sys.argv[4]})
+
+'''Get the results with method fetchall()'''
+
+results = cursor.fetchall()
+
+'''Print the results'''
+for raw in results:
+    print(raw)
+
+'''Disconect'''
+database.close()
