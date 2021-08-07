1b9e00ffc3b224badd406d4e6bfd71816c354f80
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-07 20:32:30 +0000

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,25 +4,25 @@ import MySQLdb
 import sys
 
 '''Open database conection'''
+if __name__ = "__main__":
+    database = MySQLdb.connect("localhost", sys.argv[1],
+                               sys.argv[2], sys.argv[3], port=3306)
 
-database = MySQLdb.connect("localhost", sys.argv[1],
-                           sys.argv[2], sys.argv[3], port=3306)
+    cuar = sys.argv[4]
+    '''Cursor object with cursor() method'''
+    cursor = database.cursor()
 
-cuar = sys.argv[4]
-'''Cursor object with cursor() method'''
-cursor = database.cursor()
+    '''Consulte the table and execute commands with execute() method'''
+    cursor.execute("SELECT * FROM states WHERE name = %(n)s ORDER BY id ASC",
+                   {'n': sys.argv[4]})
 
-'''Consulte the table and execute commands with execute() method'''
-cursor.execute("SELECT * FROM states WHERE name = %(u)s ORDER BY id ASC",
-               {'u': sys.argv[4]})
+    '''Get the results with method fetchall()'''
 
-'''Get the results with method fetchall()'''
+    results = cursor.fetchall()
 
-results = cursor.fetchall()
+    '''Print the results'''
+    for raw in results:
+        print(raw)
 
-'''Print the results'''
-for raw in results:
-    print(raw)
-
-'''Disconect'''
-database.close()
+    '''Disconect'''
+    database.close()
