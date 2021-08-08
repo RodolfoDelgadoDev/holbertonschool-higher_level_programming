932da26c79f2ebf704dc22308f48ac3de43cd78d
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-08 20:06:13 +0000

    fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-'''file that contains the class definition 
+'''file that contains the class definition
 State and an instance base = declarative_base()'''
 from sqlalchemy.ext.declarative_base import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 '''State Class'''
+
+
 class State(Base):
     '''State'''
     __tablename__ = 'states'
