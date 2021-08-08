9bfacbc621eff7c4abc6e9049b621e08b1f73417
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-08 22:39:52 +0000

    added ex 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''script that lists all State objects from the database hbtn_0e_6_usa'''
+import sys
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+'''Create a conection'''
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                       format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+'''Makes a session'''
+Session = sessionmaker(engine)
+session = Session()
+''' Loop to print all the table '''
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+session.close()
