c5c8de434a66ffba04b84ece81f9d40022e01a7f
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-09 00:18:32 +0000

    added ex 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''''''
+import sys
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+'''Create a conection'''
+if __name__ == "__main__":
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    '''Makes a session'''
+    Session = sessionmaker(engine)
+    session = Session()
+    '''Loop to print all the table'''
+    s = session.query(State).filter(State.name == sys.argv[4]).first()
+    if s:
+        print("{}".format(s.id))
+    else:
+        print("Not found")
+    session.close()
