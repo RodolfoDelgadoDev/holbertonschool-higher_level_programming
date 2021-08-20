cceb8abd07e46784455eb2bb1cd375b64d7574c4
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-20 23:22:08 +0000

    added ex 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 """script that takes in a URL and an email, sends a POST request to the passed URL"""
 import urllib.request
+import urllib.request
 import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    em = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(em).encode()
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode())
