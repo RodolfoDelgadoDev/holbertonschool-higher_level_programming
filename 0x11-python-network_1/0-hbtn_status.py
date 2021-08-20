0ecf1eef8ac4ca91a6436b96da348da696bf3b4a
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-20 19:28:08 +0000

    added ex 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" script that fetches https://intranet.hbtn.io/status """
+import urllib.request
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
