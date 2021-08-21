c1fc7ee40a20ada8eae3986be374fbd563556b47
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-21 01:51:56 +0000

    added ex 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
