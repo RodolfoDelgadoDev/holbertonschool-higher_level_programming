5acdcede076f6f6e09e719be842bb24d1f736573
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-21 02:24:39 +0000

    added ex 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request"""
+import requests
+import sys
+
+if __name__ == "__main__":
+        r = requests.get(sys.argv[1])
+        if (r.status_code < 400):
+            print(r.text)
+        else:
+            print("Error code: {}".format(r.status_code))
