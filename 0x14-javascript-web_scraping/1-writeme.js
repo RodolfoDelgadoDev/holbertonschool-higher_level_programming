bd142ed5a2c5adb410082f9cdd796aef736e6cae
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-14 20:24:52 +0000

    added ex 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+const fs = require('fs');
+fs.writeFile(args[2], args[3], 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
