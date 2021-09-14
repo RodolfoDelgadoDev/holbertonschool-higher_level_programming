7eee6d977da7600dc3ce6ab9730db88649172327
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-14 21:01:31 +0000

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const args = process.argv;
 const request = require('request');
 request(args[2], (err, res, body) => {
   if (err) { return console.log(err); }
-  console.log('code ' + res.statusCode);
+  console.log('code: ' + res.statusCode);
 });
